4618ee56647595d84998876dd621b6d6828392bf
nicola00 <nicolajobs00@gmail.com>
2023-01-31 15:17:29 +0100

    FEAT prezzo biglietto e prezzo scontato del 20%

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 // 1. Chiedre all'utente i km che vuole percorrere
+let kmPercorsi = parseInt ( prompt ('Quanti km vuoi percorrere?'));
+console.log (kmPercorsi)
 // 2. Chiedre all'utente l'eta
+let eta = prompt ('Quanti anni hai?')
+console.log(eta)
 // 3. Trovare il prezzo del biglietto (0.21€)
+let prezzoBiglietto = kmPercorsi * 0.21
+console.log(prezzoBiglietto)
 // 4. inserire il 20% di sconto per gli under 18
+let scontoUnder = (prezzoBiglietto * 20) / 100
+let prezzoScontoUnder = prezzoBiglietto - scontoUnder
+console.log(scontoUnder)
+console.log(prezzoScontoUnder)
 // 5. Inserire il 40% di sconto per gli over 65
 // 6. Calcolare il prezzo del biglietto in base ai dati sopra trovati
 
