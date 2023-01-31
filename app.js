08955c94d8bdf0745c62cffc36ef461947d16492
nicola00 <nicolajobs00@gmail.com>
2023-01-31 15:22:00 +0100

    FEAT prezzo scontato trovato

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ let prezzoBiglietto = kmPercorsi * 0.21
 console.log(prezzoBiglietto)
 // 4. inserire il 20% di sconto per gli under 18
 let scontoUnder = (prezzoBiglietto * 20) / 100
-let prezzoScontoUnder = prezzoBiglietto - scontoUnder
 console.log(scontoUnder)
-console.log(prezzoScontoUnder)
 // 5. Inserire il 40% di sconto per gli over 65
-// 6. Calcolare il prezzo del biglietto in base ai dati sopra trovati
-
+let scontoOver = (prezzoBiglietto * 40) / 100
+console.log(scontoOver)
+// 6. Calcolare il prezzo scontato del biglietto in base ai dati sopra trovati
+let prezzoScontoUnder = prezzoBiglietto - scontoUnder
+console.log(prezzoScontoUnder)
+let prezzoScontoOver = prezzoBiglietto - scontoOver
+console.log (prezzoScontoOver)
