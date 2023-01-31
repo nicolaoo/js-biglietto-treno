990faec523b437472387c2507fb09a3c65bb14cd
nicola00 <nicolajobs00@gmail.com>
2023-01-31 14:58:22 +0100

    FEAT lista

---
+++ app.js
@@ -0,0 +1,7 @@
+// 1. Chiedre all'utente i km che vuole percorrere
+// 2. Chiedre all'utente l'eta
+// 3. Trovare il prezzo del biglietto (0.21€)
+// 4. inserire il 20% di sconto per gli under 18
+// 5. Inserire il 40% di sconto per gli over 65
+// 6. Calcolare il prezzo del biglietto in base ai dati sopra trovati
+
